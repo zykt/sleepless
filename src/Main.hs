bc0750fd219f7109327aa2434a1e663fb0f6c6cb
ian <zyktnn@gmail.com>
2018-01-28 02:41:57 +0300

    Move parser into own module

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,8 @@
 module Main where
 
-import Data.Void
-import Control.Monad
-import Text.Megaparsec
-import Text.Megaparsec.Char
 
-type Parser = Parsec Void String
+import Control.Monad
+import Parser
 
 data Expr
     = Number Integer
@@ -13,66 +10,10 @@ data Expr
     | Parens [Expr]
     deriving (Show)
 
-data BasicToken
-    = TokenNumber Integer
-    | TokenDouble Double
-    | TokenIdent  String
-    | TokenLeftParen
-    | TokenRightParen
-    deriving (Show)
-
 main :: IO ()
 main = do
     putStr "> "
     str <- getLine
-    let result = show $ parse tokensParser "" str
+    let result = show $ parseTokens str
     putStrLn result
     when (str /= "(exit)") main
-
-
-symbol :: Parser Char
-symbol = oneOf "!$%&*+-./:<=>?@^_~"
-
---
--- Token parsers
---
-
-numberParser :: Parser BasicToken
-numberParser = (TokenNumber . read) <$> some digitChar
-
-doubleParser :: Parser BasicToken
-doubleParser = do
-        n1 <- some digitChar
-        char '.'
-        n2 <- some digitChar
-        return $ TokenDouble . read $ n1 ++ "." ++ n2
-
-identParser :: Parser BasicToken
-identParser = TokenIdent <$> parser
-    where parser = (:) <$> (letterChar <|> symbol)
-                       <*> many (letterChar <|> symbol <|> digitChar)
-
-rightParenParser :: Parser BasicToken
-rightParenParser = char ')' *> pure TokenRightParen
-
-leftParenParser :: Parser BasicToken
-leftParenParser = char '(' *> pure TokenLeftParen
-
--- terminator for lookAhead termination of elements
-terminatorParser :: Parser ()
-terminatorParser = (oneOf "() " *> pure ()) <|> (eof *> pure ())
-
--- works:
--- > parse tokensParser "" "(+ 2 s2 35)"
--- Right [Parens [Ident "+",Number 2,Ident "s2",Number 35]]
-tokensParser :: Parser [BasicToken]
-tokensParser = space
-             *> some ((parensParser <|> elementParser) <* space)
-    where
-        parensParser = leftParenParser <|> rightParenParser
-        elementParser = choice elements
-        elements = map (\p -> try $ p <* lookAhead terminatorParser)
-            [ numberParser
-            , identParser
-            , doubleParser
-            ]
