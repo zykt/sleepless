3faf48b0c2a26447791fbf413e3d3276500d096c
ian <zyktnn@gmail.com>
2018-01-28 05:10:21 +0300

    Add logic for creation of syntax tree

--- src/Main.hs
+++ src/Main.hs
@@ -4,16 +4,46 @@ module Main where
 import Control.Monad
 import Parser
 
+
+
 data Expr
-    = Number Integer
+    = IntegerNumber Integer
+    | DoubleNumber Double
     | Ident  String
     | Parens [Expr]
+    deriving (Show)
+
+
+data SyntaxError = SyntaxError String
     deriving (Show)
 
 main :: IO ()
 main = do
     putStr "> "
     str <- getLine
-    let result = show $ parseTokens str
-    putStrLn result
+    let tokens = parseTokens str
+    let result = fmap createTree tokens
+    print $ show tokens
+    print $ show result
     when (str /= "(exit)") main
+
+
+-- Build recursively expression tree
+-- separating regions defined by parenthesis into their own subnodes
+createTree :: [BasicToken] -> Either SyntaxError [Expr]
+createTree [] = Right []
+createTree (t:tokens) = case t of
+    TokenNumber n ->
+        (:) <$> pure (IntegerNumber n) <*> createTree tokens
+    TokenDouble d ->
+        (:) <$> pure (DoubleNumber d) <*> createTree tokens
+    TokenIdent  i ->
+        (:) <$> pure (Ident i) <*> createTree tokens
+    TokenLeftParen ->
+        (:) <$> (Parens <$> createParens tokens) <*> createTree tokens
+    TokenRightParen ->
+        Right []
+
+createParens :: [BasicToken] -> Either SyntaxError [Expr]
+createParens [] = Left $ SyntaxError "Right parenthesis exprected"
+createParens ts = createTree ts
