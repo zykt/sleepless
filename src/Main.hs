cd0d86614e29cb4d1d424cb04684e2d00dd70c2b
ian <zyktnn@gmail.com>
2018-01-29 06:30:31 +0300

    Implement simple evaluation

--- src/Main.hs
+++ src/Main.hs
@@ -15,9 +15,10 @@ data Expr
     deriving (Show)
 
 
-data SyntaxError
+data Error
     = SyntaxError String
     | TokenParseError (ParseError Char Void)
+    | EvalError String
     deriving (Show)
 
 
@@ -26,18 +27,53 @@ main = do
     putStr "> "
     str <- getLine
     let tokens = BF.first adapter $ parseTokens str
-    let result = createTree =<< tokens
+    let exprs = createTree =<< tokens
+    let result = eval =<< exprs
     print $ show tokens
+    print $ show exprs
+    print "Result:"
     print $ show result
     when (str /= "(exit)") main
     where
-        adapter :: ParseError Char Void -> SyntaxError
+        adapter :: ParseError Char Void -> Error
         adapter e = TokenParseError e
+
+
+eval :: [Expr] -> Either Error [Expr]
+eval [] = Right []
+eval (expr:rest) = case expr of
+    IntegerNumber i ->
+        (:) <$> pure (IntegerNumber i) <*> eval rest
+    DoubleNumber d ->
+        (:) <$> pure (DoubleNumber d) <*> eval rest
+    Ident i ->
+        (:) <$> pure (Ident i) <*> eval rest
+    Parens content ->
+        (:) <$> evalProcedure content <*> eval rest
+
+evalProcedure :: [Expr] -> Either Error Expr
+evalProcedure [] = Left $ EvalError "Can not evaluate empty proc"
+evalProcedure (f:args) = case f of
+    Ident "+" -> specialSum args
+    _ -> Left $ EvalError "Not implemented"
+
+
+specialSum :: [Expr] -> Either Error Expr
+specialSum [] = Right $ IntegerNumber 0
+specialSum (a:args) = case a of
+    n@(IntegerNumber _) ->
+        (s n) =<< specialSum args
+    e ->
+        Left $ EvalError ("Unexpected argument to sum" ++ show e)
+    where
+        s :: Expr -> Expr -> Either Error Expr
+        s (IntegerNumber n1) (IntegerNumber n2) = Right $ IntegerNumber (n1 + n2)
+        s e1 e2 = Left $ EvalError ("Unexpected argument to sum" ++ show e1 ++ " " ++ show e2)
 
 
 -- Build recursively expression tree
 -- separating regions defined by parenthesis into their own subnodes
-createTree :: [BasicToken] -> Either SyntaxError [Expr]
+createTree :: [BasicToken] -> Either Error [Expr]
 createTree [] = Right []
 createTree (t:tokens) = case t of
     TokenNumber n ->
@@ -51,6 +87,6 @@ createTree (t:tokens) = case t of
     TokenRightParen ->
         Right []
 
-createParens :: [BasicToken] -> Either SyntaxError [Expr]
+createParens :: [BasicToken] -> Either Error [Expr]
 createParens [] = Left $ SyntaxError "Right parenthesis exprected"
 createParens ts = createTree ts
