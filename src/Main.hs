1cd04680ba180de2cab63041405c33e56f42abb7
ian <zyktnn@gmail.com>
2018-02-03 00:12:47 +0300

    Add another evaluation engine

--- src/Main.hs
+++ src/Main.hs
@@ -11,23 +11,58 @@ import Data.Void
 import Parser
 
 
+-- Basic element of Expr
 data Atom
     = AtomInteger Integer
     | AtomDouble  Double
-    | AtomIdent   String
+    | AtomIdent   Ident
     deriving (Show)
 
 
+-- Representation of program in terms of expressions
 data Expr
     = ExprAtom   Atom
     | ExprParens [Expr]
     deriving (Show)
 
 
+-- Internal representation of program
 data Internal
     = InternalAtom Atom
     | InternalList [Internal]
-    | InternalProc
+    | InternalProc Args Expr
+    | InternalBuiltInProc Args (Func Internal (Either Error Internal))
+    | InternalCallProc Ident [Internal]
+    deriving (Show)
+
+
+simpleShowInternal :: Internal -> String
+simpleShowInternal i = case i of
+    InternalAtom a -> simpleShowAtom a
+    _ -> "Error! Not implemented!"
+    where
+        simpleShowAtom (AtomInteger x) = show x
+        simpleShowAtom (AtomDouble x) = show x
+        simpleShowAtom (AtomIdent x) = show x
+
+
+
+-- Recursive structure for multiple arguments of procedures
+-- always ends in either Multiarg or NoArg
+data Args
+    = Arg Args
+    | MultiArg
+    | NoArg
+    deriving (Show)
+
+
+data Func a b
+    = Func1 (a -> b)
+    | Func2 (a -> a -> b)
+
+instance Show (Func a b) where
+    show (Func1 _) = "Func1"
+    show (Func2 _) = "Func2"
 
 
 data Error
@@ -37,7 +72,22 @@ data Error
     deriving (Show)
 
 
-type Env = Map String Internal
+type Ident = String
+
+type Env = Map Ident Internal
+
+
+defaultEnv :: Env
+defaultEnv = Map.singleton "add" (builtInBinaryProc builtInSum)
+    where
+        builtInBinaryProc proc = InternalBuiltInProc binaryArgs (Func2 proc)
+        binaryArgs = Arg (Arg NoArg)
+
+
+builtInSum :: Internal -> Internal -> Either Error Internal
+builtInSum (InternalAtom (AtomInteger i1)) (InternalAtom (AtomInteger i2)) =
+    pure $ InternalAtom (AtomInteger (i1 + i2))
+builtInSum _ _ = Left $ EvalError "Unexpected argument to sum"
 
 
 main :: IO ()
@@ -46,24 +96,30 @@ main = do
     str <- getLine
     let tokens = BF.first TokenParseError $ parseTokens str
     let exprs = createExprTree =<< tokens
-    let result = eval =<< exprs
+    let result = eval defaultEnv =<< exprs
+    let internal = internalRepr =<< exprs
+    let internalEval = evalInternal defaultEnv =<< internal
     print $ show tokens
     print $ show exprs
-    print "Result:"
     print $ show result
+    print $ show internal
+    print $ show internalEval
+    case internalEval of
+        Right xs -> forM_ xs (print . simpleShowInternal)
+        Left err -> print . show $ err
     when (str /= "(exit)") main
 
 --
 -- Evaluation
 --
 
-eval :: [Expr] -> Either Error [Expr]
-eval [] = Right []
-eval (expr:rest) = case expr of
+eval :: Env -> [Expr] -> Either Error [Expr]
+eval _ [] = Right []
+eval e (expr:rest) = case expr of
     a@(ExprAtom _) ->
-        (:) <$> pure a <*> eval rest
+        (:) <$> pure a <*> eval e rest
     ExprParens content ->
-        (:) <$> evalProcedure content <*> eval rest
+        (:) <$> evalProcedure content <*> eval e rest
 
 
 evalProcedure :: [Expr] -> Either Error Expr
@@ -84,6 +140,60 @@ specialSum (a:args) = case a of
         s :: Expr -> Expr -> Either Error Expr
         s (ExprAtom (AtomInteger n1)) (ExprAtom (AtomInteger n2)) = Right $ ExprAtom $ AtomInteger (n1 + n2)
         s e1 e2 = Left $ EvalError ("Unexpected argument to sum" ++ show e1 ++ " " ++ show e2)
+
+
+internalRepr :: [Expr] -> Either Error [Internal]
+internalRepr [] = pure []
+internalRepr (e:exprs) = case e of
+    ExprAtom a ->
+        (:) <$> pure (InternalAtom a) <*> internalRepr exprs
+    ExprParens (name:args) ->
+        (:) <$> procCallFromParens name args <*> internalRepr exprs
+    where
+        ident :: Expr -> Either Error Ident
+        ident (ExprAtom (AtomIdent i)) = Right i
+        ident _ = Left $ EvalError "Expected identificator"
+
+        procCallFromParens :: Expr -> [Expr] -> Either Error Internal
+        procCallFromParens nameExpr args =
+            let name = case nameExpr of
+                    (ExprAtom (AtomIdent i)) -> Right i
+                    _ -> Left $ EvalError "Expected identificator"
+            in InternalCallProc <$> name <*> internalRepr args
+
+
+evalInternal :: Env -> [Internal] -> Either Error [Internal]
+evalInternal _ [] = pure []
+evalInternal env (x:xs) = case x of
+    a@(InternalAtom _) ->
+        (:) <$> pure a <*> evalInternal env xs
+    InternalCallProc name args ->
+        (:) <$> join (callProc env name <$> (evalInternal env args)) <*> evalInternal env xs
+
+
+lookupEnv :: Env -> Ident -> Either Error Internal
+lookupEnv env name = case Map.lookup name env of
+    Nothing -> Left $ EvalError ("Undefined reference " ++ name)
+    Just x -> Right x
+
+
+callProc :: Env -> Ident -> [Internal] -> Either Error Internal
+callProc env name args = helper =<< (lookupEnv env name)
+    where
+        helper :: Internal -> Either Error Internal
+        helper = \case
+            InternalBuiltInProc procArgs f ->
+                applyFunc f =<< format args procArgs
+        format (x:xs) (Arg rest) = (:) <$> pure x <*> format xs rest
+        format [] NoArg = pure []
+        format xs MultiArg = pure xs
+        format _ _ = Left $ EvalError "Wrong arguments"
+
+
+applyFunc :: Func Internal (Either Error Internal) -> [Internal] -> Either Error Internal
+applyFunc (Func1 f) (x1:_) = f x1
+applyFunc (Func2 f) (x1:x2:_) = f x1 x2
+applyFunc _ _ = Left $ EvalError "Built-in Function application error"
 
 --
 -- Recursive reading of tokens
