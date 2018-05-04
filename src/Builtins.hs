98b10b50d75d49c913863561d420619c261d881d
ian <zyktnn@gmail.com>
2018-05-04 22:05:14 +0300

    New builtins

--- src/Builtins.hs
+++ src/Builtins.hs
@@ -11,6 +11,12 @@ defaultEnv =
                  ,("mul", integerBinOp (*))
                  ,("minus", integerBinOp (-))
                  ,("div", integerBinOp div)
+                 ,(">", integerCompOp (>))
+                 ,("<", integerCompOp (<))
+                 ,(">=", integerCompOp (>=))
+                 ,("<=", integerCompOp (<=))
+                 ,("=", integerCompOp (==))
+                 ,("!=", integerCompOp (/=))
                  ,("integer?", predicate integerPredicate)
                  ]
 
@@ -22,8 +28,8 @@ predicate :: (Internal -> Either Error Internal) -> Internal
 predicate p = InternalBuiltInProc unaryArgs $ Func1 p
 
 integerPredicate :: Internal ->  Either Error Internal
-integerPredicate (InternalAtom (AtomInteger _)) = pure $ InternalAtom (AtomSpecial "#t")
-integerPredicate _ = pure $ InternalAtom (AtomSpecial "#f")
+integerPredicate (InternalAtom (AtomInteger _)) = pure $ InternalAtom (AtomSpecial "t")
+integerPredicate _ = pure $ InternalAtom (AtomSpecial "f")
 
 
 binaryArgs :: Args ()
@@ -36,3 +42,11 @@ integerBinOpFunc _ _ _ = Left $ EvalError "Unexpected argument to int function"
 
 integerBinOp :: (Integer -> Integer -> Integer) -> Internal
 integerBinOp op = InternalBuiltInProc binaryArgs (Func2 $ integerBinOpFunc op)
+
+integerCompOpFunc :: (Integer -> Integer -> Bool) -> Internal -> Internal -> Either Error Internal
+integerCompOpFunc op (InternalAtom (AtomInteger i1)) (InternalAtom (AtomInteger i2))
+    | op i1 i2 = pure $ InternalAtom (AtomSpecial "t")
+    | otherwise = pure $ InternalAtom (AtomSpecial "f")
+
+integerCompOp :: (Integer -> Integer -> Bool) -> Internal
+integerCompOp op = InternalBuiltInProc binaryArgs (Func2 $ integerCompOpFunc op)
